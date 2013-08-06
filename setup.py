8df1c3880fe4e6f8ac55a496cd1eddfe45a36ab3
Jingchao <jingchao.di@duitang.com>
2013-08-06 16:57:27 +0800

    update version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name='weby',
-    version='0.1.2',
+    version='0.1.4',
     description='WSGI Server for Donkey',
     author='ddd',
     author_email='jingchao.di@duitang.com',
